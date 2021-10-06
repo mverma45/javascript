d44ff3da2880ecf808a8b8f713edb1da95ee474c
Manny Verma <mverma7845@gmail.com>
2021-10-05 19:30:23 -0600

    this doesn't work

---
+++ main.js
@@ -0,0 +1,43 @@
+//Random Number Generator
+function random(newNum) {
+    return Math.floor(Math.random());
+}
+console.log(Math.floor(Math.random()* 10 ) +1); 
+
+//document.getElementById
+function changeColor(newColor) {
+    let sweet = document.getElementById('clicked');
+    sweet.style.color = newColor;
+    console.log('it works HAHAHAHA')
+}
+
+function changePic(newPic) {
+    let yum = document.getElementsById('pic');
+    yum.style.newPic = newPic;
+}
+
+//document.querySelector
+let newImage = document.querySelector('img');
+
+newImage.onclick = function () {
+    let mySrc = newImage.getAttribute('src');
+    if (mySrc === '../JavaScript/waffle.jpg') {
+        newImage.setAttribute('src', '../JavaScript/hamburger.jpg');
+    } else {
+        newImage.setAttribute('src', '../JavaScript/waffle.jpg')
+        console.log('it worked')
+    }
+}
+
+
+// document.querySelectorAll
+// const inner = select.querySelectorAll('p')
+function find() {
+    let x
+    let i
+    x = document.querySelectorAll('p');
+    for (i = 0; i < x.length; i++){
+        x[i].style.backgroundColor = "blue"
+    }
+}
+
